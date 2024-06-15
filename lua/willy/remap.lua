877ee241631de36688ada08b9aaa87391ee3552b
WillyHB <william.hansen.baird@icloud.com>
2024-06-16 00:09:24 +0200

    Remapped s&r and window management

--- lua/willy/remap.lua
+++ lua/willy/remap.lua
@@ -1,16 +1,38 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e<leader>", vim.cmd.Ex)
+vim.g.mapleader = " " vim.keymap.set("n", "<leader>e<leader>", vim.cmd.Ex)
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n",  "n", "nzzzv")
 vim.keymap.set("n", "N", "nzzzv")
 
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+--vim.keymap.set("x", "<leader>p", "\"_dP")
+--SEARCH AND REPLACE
+vim.g.mapleader = "\\"
+--Search for current word on line and replace
+vim.keymap.set("n", "<leader>s", ":s/\\<<C-r><C-w>\\>//gc<Left><Left><Left>")
+--Search for current word in file and replace
+vim.keymap.set("n", "<leader>ss", ":%s/\\<<C-r><C-w>\\>//gc<Left><Left><Left>")
+--Search for current selection in line and replace
+vim.keymap.set("v", "<leader>s", ":s/<C-r>h//gc<Left><Left><Left>")
 
-vim.keymap.set("x", "<leader>p", "\"_dP")
+--Setup for search and replace in line
+vim.keymap.set("n", "<leader>r", ":s//gc<Left><Left><Left>")
+--Setup for search and replace in file
+vim.keymap.set("n", "<leader>rr", ":%s//gc<Left><Left><Left>")
+--Setup for search and replace for selection in line
+vim.keymap.set("v", "<leader>r", ":s//gc<Left><Left><Left>")
 
---vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left>")
-vim.keymap.set("n", "<leader>s", ":%s%<C-r><C-w>%") 
+--BUFFERS/WINDOWS/TABS
+vim.g.mapleader = "n"
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+vim.keymap.set("n", "<leader>o", "<C-w>s")
+vim.keymap.set("n", "<leader>p", "<C-w>v")
+vim.keymap.set("n", "<leader>q", "<C-w>q")
+vim.keymap.set("n", "<leader>H", "<C-w>H")
+vim.keymap.set("n", "<leader>J", "<C-w>J")
+vim.keymap.set("n", "<leader>K", "<C-w>K")
+vim.keymap.set("n", "<leader>L", "<C-w>L")
+vim.keymap.set("n", "<leader>w", "<C-w>w")
+
--- lua/willy/set.lua
+++ lua/willy/set.lua
@@ -4,7 +4,8 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.wrap = false;
+vim.opt.wrap = true;
+vim.g.showbreak = '↪'
 vim.opt.smartindent = true;
 
 
@@ -13,3 +14,4 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 vim.o.scrolloff = 8
+vim.o.syntax = 'enable'
